dd635335eb3799a5fd634fd0269f8e77e0af1f2c
ajie <ajie@github.com>
2018-05-03 17:39:27 +0800

    1

---
+++ code/client/Swift/SSIMMessage/SSIMMessage/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SSIMMessage
+//
+//  Created by 阿杰 on 2018/5/2.
+//  Copyright © 2018年 阿杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
