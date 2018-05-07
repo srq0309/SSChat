5ae9b7a1c0c4be988749b60c2b22e7a4f8785146
ajie <ajie@github.com>
2018-05-07 13:33:43 +0800

    。。

--- code/client/Object-C/SSIMMessage/SSIMMessage/ViewController.m
+++ code/client/Object-C/SSIMMessage/SSIMMessage/ViewController.m
@@ -18,6 +18,8 @@
     BOOL canSendMessage;
 }
 @property (weak, nonatomic) IBOutlet UIView *bgview;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTx;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTx;
 @end
 
 @implementation ViewController
@@ -42,11 +44,21 @@
     NSLog(@"connectClose");
 }
 
+-(void)Login{
+    NSLoginMessage * message = [[NSLoginMessage alloc]init];
+    message.userName = _usernameTx.text;
+    message.passWard = _passwordTx.text;
+    [manager sendMessage:message.getMessageData];
+}
+
 - (void)onSocketConnectWithResult:(BOOL)isSuccess {
     if (isSuccess) {
         NSLog(@"connectSuccess");
+        [self Login];
     }else{
         NSLog(@"connectFaile");
+        UIAlertController * alc = [UIAlertController alertControllerWithTitle:@"提示" message:@"连接超时" preferredStyle:UIAlertControllerStyleAlert];
+        [self presentViewController:alc animated:YES completion:nil];
     }
 }
 - (IBAction)sendMessage:(id)sender {
@@ -68,10 +80,14 @@
     [factory registMessageWithClass:NSInstantMessage.class andSubType:NSMESSAGE_TYPE_INSTANT];
     NSMessage * message = [factory createMessageByData:messages];
     NSLog(@"%@",message.toString);
+    if (message.subType == NSMESSAGE_TYPE_LOGIN) {
+        UIAlertController * alc = [UIAlertController alertControllerWithTitle:@"提示" message:@"登录成功" preferredStyle:UIAlertControllerStyleAlert];
+        [self presentViewController:alc animated:YES completion:nil];
+    }
 }
 
 - (IBAction)connect:(id)sender {
-    [manager connectToHost:@"121.69.81.74" andPort:80];
+    [manager connectToHost:@"121.69.81.74" andPort:6677];
 }
 
 @end
